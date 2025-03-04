83d1a36ef9e36d7b9666caf102f115704e96bada
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-04 22:49:35 +0300

    Add OrderStatus enum and Status property to Order model

--- Microservices.AspNetCore.OrderService/Models/Order.cs
+++ Microservices.AspNetCore.OrderService/Models/Order.cs
@@ -1,8 +1,16 @@
 namespace Microservices.AspNetCore.OrderService.Models;
 
+public enum OrderStatus
+{
+  Pending,
+  Completed,
+  Cancelled,
+}
+
 public class Order
 {
   public int Id { get; set; }
   public int ProductId { get; set; }
   public int Quantity { get; set; }
+  public OrderStatus Status { get; set; } = OrderStatus.Pending;
 }
