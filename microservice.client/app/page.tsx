3caaa6cf78ea51841f80502220732e574aa0607f
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-04 22:47:13 +0300

    next.js client web for demonstrating eventual consistency

--- microservice.client/app/page.tsx
+++ microservice.client/app/page.tsx
@@ -1,101 +1,111 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+  thumbUrl: string;
+}
+
+function ProductCard({ id, name, price, stock, thumbUrl }: Product) {
+  const [quantity, setQuantity] = useState(1);
+  return (
+    <div className="border p-4 rounded-lg border-gray-200 hover:border-gray-400 flex  flex-col gap-2">
+      <Image
+        src={thumbUrl}
+        alt={name}
+        width={300}
+        height={300}
+        className="mx-auto object-contain flex-grow"
+      />
+      <div className="flex flex-col ">
+        <div className="font-bold">{name}</div>
+        <div className="flex justify-between">
+          <div>${price}</div>
+          <div className="border-1 p-1 px-2 rounded-md border-orange-400">
+            In Stock: {stock}
+          </div>
+        </div>
+
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, est.
+        </div>
+        <form
+          method="post"
+          onSubmit={(e) => {
+            e.preventDefault();
+            fetch("http://localhost:8081/orders", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                productId: id,
+                quantity: quantity,
+              }),
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                console.log(data);
+              });
+          }}
+          className="flex justify-end gap-3"
+        >
+          <input type="hidden" name="productId" value={id} />
+          <input
+            type="number"
+            className="border border-gray-200 rounded-md p-1 w-16"
+            defaultValue={quantity}
+            onChange={(e) => setQuantity(parseInt(e.target.value))}
+            name="quantity"
+            max={stock}
+          />
+          <input
+            type="submit"
+            className="border border-gray-200 rounded-full px-5 p-1 hover:bg-black hover:text-white text-sm"
+            value={"Order Now"}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [products, setProducts] = useState<Array<Product>>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  useEffect(() => {
+    fetch("http://localhost:8080/products", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, []);
+
+  return (
+    <div className="p-8">
+      <h1 className="text-center text-6xl m-8">Microservices ASP.NET Core</h1>
+      <div className="grid lg:grid-cols-4 grid-cols-2 mx-auto w-fit gap-10">
+        {products.map((product, key) => (
+          <ProductCard
+            id={product.id}
+            key={key}
+            name={product.name}
+            price={product.price}
+            stock={product.stock}
+            thumbUrl={product.thumbUrl}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        ))}
+      </div>
     </div>
   );
 }
--- microservice.client/next.config.ts
+++ microservice.client/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "c1.neweggimages.com",
+      "wapcomputer.com",
+      "cdn11.bigcommerce.com",
+      "www.costco.com",
+      "hk-media.apjonlinecdn.com",
+    ],
+  },
 };
 
 export default nextConfig;
